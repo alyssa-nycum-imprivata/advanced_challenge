fcc2e475eae51e3c8aceca33644c4298891126af
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-01-23 15:46:40 -0600

    finished the dom fragements part of the advanced challenge

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
-const articleElement = document.querySelector("#messages");
+const fragment = document.createDocumentFragment();
+
+// const articleElement = document.querySelector("#messages");
 
 const section1 = document.createElement('section');
 section1.className = "message";
 section1.textContent = "HELLO";
-articleElement.appendChild(section1);
+fragment.appendChild(section1);
 const section2 = document.createElement('section');
 section2.className = "message";
 section2.textContent = "WORLD";
-articleElement.appendChild(section2);
+fragment.appendChild(section2);
 const section3 = document.createElement('section');
 section3.className = "message";
 section3.textContent = "I";
-articleElement.appendChild(section3);
+fragment.appendChild(section3);
 const section4 = document.createElement('section');
 section4.className = "message";
 section4.textContent = "AM";
-articleElement.appendChild(section4);
+fragment.appendChild(section4);
 const section5 = document.createElement('section');
 section5.className = "message";
 section5.textContent = "HERE";
-articleElement.appendChild(section5);
+fragment.appendChild(section5);
 
-console.log(articleElement);
+document.querySelector("#messages").appendChild(fragment);
+
+// console.log(articleElement);
+
+
+
 
 
 
